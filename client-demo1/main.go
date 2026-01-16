3fe5360cd65a7d3ab9c2f5680492a7f07048949d
aabb <809073023@qq.com>
2026-01-16 19:47:00 +0800

    feat: add client-demo1

--- client-demo1/main.go
+++ client-demo1/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"client-demo1/proto/greeter"
+	"context"
+	"fmt"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("127.0.0.1:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+	}
+	client := greeter.NewGreeterClient(conn)
+	hello, err := client.SayHello(context.Background(), &greeter.HelloReq{
+		Name: "hello",
+	})
+	if err != nil {
+	}
+	fmt.Printf("%+v\n", hello)
 }
