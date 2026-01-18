343861ebc3917bdb5e29687538fffb32884aba69
aabb <809073023@qq.com>
2026-01-18 20:08:49 +0800

    fix: fix bug

--- client-demo2/main.go
+++ client-demo2/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hashicorp/consul/api"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -16,15 +17,23 @@ func main() {
 	// 获取consul操作对象
 	client, err := api.NewClient(config)
 	if err != nil {
+		panic(err)
 	}
 	// 获取地址
-	serviceEntry, _, _ := client.Health().Service("HelloService", "test", false, nil)
+	serviceEntry, _, err := client.Health().Service("HelloService", "test", false, nil)
+	if err != nil {
+		panic(err)
+	}
+	if len(serviceEntry) == 0 {
+		panic("no service entry for HelloService with tag test")
+	}
 	fmt.Println(serviceEntry[0].Service.Address)
 	fmt.Println(serviceEntry[0].Service.Port)
 	address := serviceEntry[0].Service.Address + ":" + strconv.Itoa(serviceEntry[0].Service.Port)
 
-	conn, err := grpc.NewClient(address)
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
+		panic(err)
 	}
 	serviceClient := greeter.NewGreetServiceClient(conn)
 	resp, err := serviceClient.SayHello(context.Background(), &greeter.HelloReq{
--- server-demo2/greeter/main.go
+++ server-demo2/greeter/main.go
@@ -25,34 +25,42 @@ func main() {
 	// 注册consul服务
 	//初始化consul配置
 	config := api.DefaultConfig()
+	config.Address = "http://127.0.0.1:8500"
 	// 获取consul操作对象
 	client, err := api.NewClient(config)
 	if err != nil {
+		panic(err)
 	}
+	serviceIP := "127.0.0.1"
+	servicePort := 9090
 	// 3、配置注册服务的参数
 	agentService := api.AgentServiceRegistration{
 		ID:      "1",
 		Tags:    []string{"test"},
 		Name:    "HelloService",
-		Port:    8080,
-		Address: "127.0.0.1",
+		Port:    servicePort,
+		Address: serviceIP,
 		Check: &api.AgentServiceCheck{
-			TCP:      "127.0.0.1:8080",
+			TCP:      serviceIP + ":9090",
 			Timeout:  "5s",
 			Interval: "30s",
 		},
 	}
 	//注册服务到consul上
-	client.Agent().ServiceRegister(&agentService)
+	if err := client.Agent().ServiceRegister(&agentService); err != nil {
+		panic(err)
+	}
 
 	server := grpc.NewServer()
 	greeter.RegisterGreetServiceServer(server, new(Hello))
-	listen, err := net.Listen("tcp", "127.0.0.1:8080")
+	listen, err := net.Listen("tcp", "127.0.0.1:9090")
 	if err != nil {
+		panic(err)
 	}
 	defer listen.Close()
 	err = server.Serve(listen)
 	if err != nil {
+		panic(err)
 	}
 
 }
