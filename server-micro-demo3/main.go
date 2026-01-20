7838999ffb276b50c702eb460afa18eba74d6a42
aabb <809073023@qq.com>
2026-01-20 09:04:46 +0800

    chore: add instruction

--- server-micro-demo3/main.go
+++ server-micro-demo3/main.go
@@ -18,14 +18,13 @@ func main() {
 	registry := consul.NewRegistry()
 	// Create service
 	service := micro.NewService(
-		micro.Name(name),
-		micro.Registry(registry),
-		micro.Version(version),
+		micro.Name(name),       // ① 服务名字（别人发现服务靠它）
+		micro.Version(version), // ② 版本号（可选，用于区分版本）
 		micro.Server(server.NewServer(
-			server.Name(name),
-			server.Registry(registry),
-			server.Address("0.0.0.0:9098"),
-			server.Advertise("127.0.0.1:9098"),
+			server.Name(name),                  // ① 同样设置服务名字（因为你自己创建了 Server）registry 时用的名字
+			server.Registry(registry),          // ③ 用哪个注册中心（Consul/Nacos 的客户端实例）
+			server.Address("0.0.0.0:9098"),     // ④ 本机监听地址：真正开端口的地方
+			server.Advertise("127.0.0.1:9098"), // ⑤ 注册到注册中心的地址：告诉别人怎么连你
 		)),
 	)
 
