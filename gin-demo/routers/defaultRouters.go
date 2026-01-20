34b9c77432a5f6672d8302e7f1715a0e6f29bd7b
aabb <809073023@qq.com>
2026-01-20 09:33:55 +0800

    chore: initialization gin-demo

---
+++ gin-demo/routers/defaultRouters.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	"gin-demo/controllers/it"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DefaultRoutersInit(r *gin.Engine) {
+	defaultRouters := r.Group("/")
+	{
+		defaultRouters.GET("/", it.DefaultController{}.Index)
+		defaultRouters.GET("/news", it.DefaultController{}.News)
+	}
+}
